1a4dadfec98e744e4b067dc38632bcaaf00bf149
Hassan <hassan.teymouri@shab.ir>
2023-10-27 19:47:18 +0330

    text init updated

--- multitts/vits/text/__init__.py
+++ multitts/vits/text/__init__.py
@@ -1,6 +1,6 @@
 """ from https://github.com/keithito/tacotron """
-from text import cleaners
-from text.symbols import symbols
+from multitts.vits.text import cleaners
+from multitts.vits.text.symbols import symbols
 
 
 # Mappings from symbol to numeric ID and vice versa:
